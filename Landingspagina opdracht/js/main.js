2944be35713d6b6557248062af7294ce1d588f70
Ruiz-M-Phang <Ruiz.M.Phang@gmail.com>
2025-04-10 12:00:44 +0200

    opdrachten_tot_nu_toe

---
+++ Landingspagina opdracht/js/main.js
@@ -0,0 +1,24 @@
+const button = document.getElementById("js_menu")
+const navigation = document.getElementById("js_nav")
+const body = document.getElementById("js_body")
+
+
+
+button.onclick = function() {
+    // navigation.style.visibility = "visible";
+    // navigation.style.opacity = "75%";
+    // body.style.overflow = "hidden";
+}
+  
+(".menu-collapsed").click = function() {
+    toggleClass("menu-expanded");
+  };
+
+/* let i = 0;
+while(i <= 10){
+    i += 1;
+}
+
+for(let i = 1; i += 10; i++){
+    console.log(i);
+} */
